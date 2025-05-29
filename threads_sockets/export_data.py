6f7b504022be6691be2fb9d0e833540e0f8347f8
unknown <allisonruiz@uninorte.edu.co>
2025-05-29 17:23:09 -0500

    Export to Feather added

--- threads_sockets/export_data.py
+++ threads_sockets/export_data.py
@@ -1,7 +1,7 @@
 import os, pandas as pd
 from sql_connection import get_data, get_connection
 from export_utils import (
-    export_csv, export_json, export_parquet, export_avro
+    export_csv, export_json, export_parquet, export_feather
 )
 #Esto se podría hacer con hilos?
 
@@ -28,28 +28,7 @@ export_json(df, "exports/ventas.json")
 # Exportar a Parquet
 export_parquet(df, "exports/ventas.parquet")
 
-#Avro: define el schema manualmente (ajusta según las columnas reales)
-avro_schema = {
-    "doc": "Ventas exportadas",
-    "name": "ventas_record",
-    "namespace": "mi.proyecto",
-    "type": "record",
-    "fields": [
-        # Asegúrate de que estos nombres y tipos coincidan con las columnas
-        # Falta verificar los tipos de las columnas al realizar las peticiones y los nombres
-        {"name": "ID_VENTA", "type": "object"},
-        {"name": "FECHA_VENTA", "type": "object"},
-        {"name": "ID_CLIENTE", "type": "int64"},
-        {"name": "ID_EMPLEADO", "type": "int64"},
-        {"name": "ID_PRODUCTO", "type": "int64"},
-        {"name": "CANTIDAD", "type": "int64"},
-        {"name": "PRECIO_UNITARIO", "type": "object"},
-        {"name": "DESCUENTO", "type": "object"},
-        {"name": "FORMA_PAGO", "type": "object"}
-    ]
-}
-
-# Exportar a Avro
-export_avro(df, "exports/ventas.avro", avro_schema)
+# Exportar a Feather
+export_feather(df, "exports/ventas.feather")
 
 print("Exportación completada correctamente ✅")
--- threads_sockets/export_utils.py
+++ threads_sockets/export_utils.py
@@ -13,7 +13,7 @@ def export_parquet(df: pd.DataFrame, path: str):
     table = pa.Table.from_pandas(df)
     pq.write_table(table, path)
 
-def export_avro(df: pd.DataFrame, path: str, schema: dict):
-    records = df.to_dict(orient="records")
-    with open(path, "wb") as out:
-        fastavro.writer(out, schema, records)
+def export_feather(df: pd.DataFrame, path: str):
+    df.to_feather(path)
+
+
